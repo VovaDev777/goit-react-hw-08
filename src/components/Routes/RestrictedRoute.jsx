e51b3d59d9baa312da943dbdf336bdd99a5a97f8
VovaDev777 <polvovater@gmail.com>
2024-08-12 23:23:56 +0200

    Update RestrictedRoute.jsx

--- src/components/Routes/RestrictedRoute.jsx
+++ src/components/Routes/RestrictedRoute.jsx
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { Navigate } from "react-router-dom";
 
 
 export default function RestrictedRoute ({component, redirectTo}) {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
-    return isLoggedIn
+    return isLoggedIn ? <Navigate to={redirectTo}/> : component;
 }
